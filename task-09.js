dfca446e8cb22b3b671ac20db12c0b3d92070ec1
ZimboCapetonian <svchitimira@gmail.com>
2021-05-19 13:15:35 +0200

    current edits to task 09 and 010

--- task-09.js
+++ task-09.js
@@ -2,5 +2,5 @@ function printVowels(string) {
     return  string.match(/[aeiouAEIOU]/ig, '');
     }
 
-    console.log(printVowels("Apples Suck"));//['e', 'o', 'o']
-    console.log(printVowels("This was hard"));//['O','U','i','o','u']
+    console.log(printVowels("Apples Suck"));
+    console.log(printVowels("This was hard"));
