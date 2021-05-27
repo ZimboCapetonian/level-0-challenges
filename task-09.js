d8cb8cfc34882d80ea7f446d0288b0d222f04b29
ZimboCapetonian <svchitimira@gmail.com>
2021-05-27 02:44:24 +0200

    Completed task 9 to the best of my ability

--- task-09.js
+++ task-09.js
@@ -1,50 +1,7 @@
-var returnVowel = "";
+function printVowels(str){
+  var matches = str.match(/[aeiouAEIOU]/gi);
+   return matches;
 
-function printVowels(x) {
-  /*switch(x) {
-    case "a":
-    case "A":
-      returnVowel = "a";
-      break;
-    case "e":
-    case "E":
-      returnVowel = "e";
-      break;
-    case "i":
-    case "I":
-      returnVowel = "i";
-      break; 
-    case "o":
-    case "O":
-      returnVowel = "o";
-      break; 
-    case "u":
-    case "U":
-      returnVowel = "u";
-      break; 
-  }
-
-  var vowelLookUp = {
-    1: "a",
-    2: "e",
-    3: "u",
-    4: "i",
-    5: "o",
-    6: "A",
-    7: "E",
-    8: "U",
-    9: "I",
-    10: "O",
-  }
-
-  console.log(vowelLookUp[1])
-  
-
-  //return `${returnVowel}`;
-  */
 }
 
-
-
-//printVowels("Apples Suck", );
-//(printVowels("This was hard"));
+ console.log(printVowels("Apples"))
