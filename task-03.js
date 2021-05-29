77be9b29af6f662d563a9110eea1217d4d5281fa
ZimboCapetonian <svchitimira@gmail.com>
2021-05-29 13:39:12 +0200

    deleted the following files

--- task-03.js
+++
@@ -1,5 +0,0 @@
-function hello(name) {
-	console.log('Hello ' + name + "!");
-}
-
-hello("Tshepo");
--- task-05.js
+++
@@ -1,9 +0,0 @@
-
-var area = function areaOfTriangle(side1, side2, side3) {
-        var x = (side1 + side2 + side3) / 2;
-        var y = Math.sqrt(x * (x - side1) * (x - side2) * (x - side3));
-        return y;
-
-}
-
-console.log(area(10, 22, 25));
