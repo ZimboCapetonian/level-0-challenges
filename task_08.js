4f81bda179c0f086f9a04d72cc52df23650bde35
ZimboCapetonian <svchitimira@gmail.com>
2021-06-01 13:26:55 +0200

    fixed the problem in task 8

--- task_08.js
+++ task_08.js
@@ -2,9 +2,20 @@ function minutesToHours(d) {
     var digit = d;
     var minutes = digit % 60;
     var hours = Math.floor(digit / 60);
-        
-    return digit + " will become " + hours + " hours, " + minutes + " minutes.";
-
+    if (hours > 1 && minutes > 1) {
+        return digit + " will become " + hours + " hours, " + minutes + " minutes.";
+    } else if (hours > 1 && minutes === 1) {
+        return digit + " will become " + hours + " hours, " + minutes + " minute.";
+    } else if (hours < 1 && minutes === 1) {
+      return digit + " will become " + minutes + " minute.";
+    } else if (hours === 1 && minutes === 1) {
+      return digit + " will become " + hours + " hour, " + minutes + " minute.";
+    }
+    else {
+        return digit + " will become " + minutes + " minutes.";
+    }
+    
 }
 
 console.log(minutesToHours(347));
+console.log(minutesToHours(61));
