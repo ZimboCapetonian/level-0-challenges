42074b2fc4e915e9a438dd65658ae5b30b55e6eb
ZimboCapetonian <svchitimira@gmail.com>
2021-06-02 14:35:12 +0200

    I fixed it to accept all types of number value variations and romves the 'd'

--- task_08.js
+++ task_08.js
@@ -1,17 +1,20 @@
-function minutesToHours(d) {
-    var digit = d;
+function minutesToHours(digit) {
     var minutes = digit % 60;
     var hours = Math.floor(digit / 60);
     if (hours > 1 && minutes > 1) {
         return digit + " will become " + hours + " hours, " + minutes + " minutes.";
-    } else if (hours >= 1 && minutes === 1) {
+    } else if (hours > 1 && minutes === 1) {
         return digit + " will become " + hours + " hours, " + minutes + " minute.";
     } else if (hours < 1 && minutes === 1) {
       return digit + " will become " + minutes + " minute.";
     } else if (hours == 1 && minutes == 1) {
       return digit + " will become " + hours + " hour, " + minutes + " minute.";
-    } else if (hours ==1 && minutes > 1) {
+    } else if (hours == 1 && minutes > 1) {
       return digit + " will become " + hours + " hour, " + minutes + " minutes."; 
+    } else if (hours == 1 && minutes == 0) {
+      return digit + " will become " + hours + " hour."; 
+    } else if (hours > 1 && minutes == 0) {
+      return digit + " will become " + hours + " hours."; 
     }
     else {
         return digit + " will become " + minutes + " minutes.";
@@ -22,3 +25,7 @@ function minutesToHours(d) {
 console.log(minutesToHours(347));
 console.log(minutesToHours(71));
 console.log(minutesToHours(61));
+console.log(minutesToHours(41));
+console.log(minutesToHours(60));
+console.log(minutesToHours(181));
+console.log(minutesToHours(180));
